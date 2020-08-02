75701b7585d2127e0edb0a714802037aa82e933b
Mansi Agarwal <agarwalmansi2911@gmail.com>
2020-08-03 00:03:14 +0530

    Added filtering recipes feature

--- recipe_store/recipe_app/views.py
+++ recipe_store/recipe_app/views.py
@@ -46,9 +46,28 @@ class RecipeViewSet(viewsets.ModelViewSet, mixins.ListModelMixin):
 
     queryset = Recipe.objects.all()
 
+    # _before_function_name(): intended to be private
+    def _params_to_ints(self, qs):
+        """Convert a comma separated list of string IDs to a list of Integers"""
+        return [int(str_id) for str_id in qs.split(',')]
+
     def get_queryset(self):
         """Retrieve the recipes for the authenticated user"""
-        return self.queryset.filter(user=self.request.user).order_by('-id')
+        # Retrieve get parameters from request is query_params dictionary
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset
+        # if tags list is not null, then convert the list to IDs
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            # tags__id__in : __ django syntax for filtering foreign key objects
+            queryset = queryset.filter(tags__id__in=tag_ids)
+
+        if ingredients:
+            ingredients_ids = self._params_to_ints(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredients_ids)
+
+        return queryset.filter(user=self.request.user).order_by('-id')
 
     def get_serializer_class(self):
         """Return appropriate serializer class"""
